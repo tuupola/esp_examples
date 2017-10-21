e2b69f5be77b2c4b984e140c6cb6cd1992b0abde
Mika Tuupola <tuupola@appelsiini.net>
2017-10-22 02:23:53 +0300

    Read button values using ISR queue

--- m5stack_button/main/main.c
+++ m5stack_button/main/main.c
@@ -2,43 +2,50 @@
 #include "esp_log.h"
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
+#include "freertos/queue.h"
 #include "stdio.h"
 
 #include "sdkconfig.h"
 
-static const char* TAG = "hello_world";
+#define ESP_INTR_FLAG_DEFAULT 0
 
-void button_a_task(void *pvParameters)
+static const char* TAG = "gpio_button";
+static xQueueHandle gpio_evt_queue = NULL;
+
+// The pin ISR handlers no longer need to be declared with IRAM_ATTR, unless
+// you pass the ESP_INTR_FLAG_IRAM flag when allocating the ISR in
+// gpio_install_isr_service().
+
+static void IRAM_ATTR gpio_isr_handler(void *arg)
 {
-    while(1) {
-        ESP_LOGD(TAG, "Button A value is %d", gpio_get_level(GPIO_NUM_39));
-        vTaskDelay(250 / portTICK_RATE_MS);
-    }
-
-    vTaskDelete(NULL);
+    uint32_t gpio_num = (uint32_t) arg;
+    xQueueSendFromISR(gpio_evt_queue, &gpio_num, NULL);
 }
 
-void button_b_task(void *pvParameters)
+static void gpio_task(void* arg)
 {
+    uint32_t pin;
     while(1) {
-        ESP_LOGD(TAG, "Button B value is %d", gpio_get_level(GPIO_NUM_38));
-        vTaskDelay(250 / portTICK_RATE_MS);
+        if (xQueueReceive(gpio_evt_queue, &pin, portMAX_DELAY)) {
+            switch (pin) {
+                case GPIO_NUM_39:
+                    ESP_LOGD(TAG, "Button A value: %d", gpio_get_level(pin));
+                    break;
+                case GPIO_NUM_38:
+                    ESP_LOGD(TAG, "Button B value: %d", gpio_get_level(pin));
+                    break;
+                case GPIO_NUM_37:
+                    ESP_LOGD(TAG, "Button C value: %d", gpio_get_level(pin));
+                    break;
+                default:
+                    ESP_LOGD(TAG, "This should not happen...");
+                    break;
+                }
+        }
     }
-
-    vTaskDelete(NULL);
 }
 
-void button_c_task(void *pvParameters)
-{
-    while(1) {
-        ESP_LOGD(TAG, "Button C value is %d", gpio_get_level(GPIO_NUM_37));
-        vTaskDelay(250 / portTICK_RATE_MS);
-    }
-
-    vTaskDelete(NULL);
-}
-
-// ESP32 application entry point
+/* ESP32 application entry point */
 void app_main()
 {
     ESP_LOGD(TAG, "Starting...");
@@ -48,12 +55,19 @@ void app_main()
     button_config.mode         = GPIO_MODE_INPUT;
     button_config.pull_up_en   = GPIO_PULLUP_DISABLE;
     button_config.pull_down_en = GPIO_PULLDOWN_ENABLE;
-    button_config.intr_type    = GPIO_INTR_DISABLE;
+    button_config.intr_type    = GPIO_INTR_ANYEDGE;
     gpio_config(&button_config);
 
-    xTaskCreatePinnedToCore(button_a_task, "Button A task", 2048, NULL, 1, NULL, 1);
-    xTaskCreatePinnedToCore(button_b_task, "Button B task", 2048, NULL, 1, NULL, 1);
-    xTaskCreatePinnedToCore(button_c_task, "Button C task", 2048, NULL, 1, NULL, 1);
+    /* Queue which receives events from ISR */
+    gpio_evt_queue = xQueueCreate(10, sizeof(uint32_t));
+
+    /* Task which receives events from queue */
+    xTaskCreate(gpio_task, "GPIO task", 2048, NULL, 10, NULL);
+
+    gpio_install_isr_service(ESP_INTR_FLAG_DEFAULT);
+    gpio_isr_handler_add(GPIO_NUM_39, gpio_isr_handler, (void*) GPIO_NUM_39);
+    gpio_isr_handler_add(GPIO_NUM_38, gpio_isr_handler, (void*) GPIO_NUM_38);
+    gpio_isr_handler_add(GPIO_NUM_37, gpio_isr_handler, (void*) GPIO_NUM_37);
 
     while(1);
 }
