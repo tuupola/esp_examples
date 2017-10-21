e16baed4c1418b55a4a4031723b12c172d903748
Mika Tuupola <tuupola@appelsiini.net>
2017-10-21 16:32:46 +0300

    Simplest Hello World in the world

---
+++ hello_world/main/main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <freertos/FreeRTOS.h>
+#include <freertos/task.h>
+
+// ESP32 application entry point
+void app_main()
+{
+    printf("Hello world!\n");
+
+    while(1);
+}
