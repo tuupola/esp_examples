63ef4217402c6ab6c86a3143c2a3f808c4abe6ae
Mika Tuupola <tuupola@appelsiini.net>
2017-10-21 23:46:39 +0300

    Basic task scheduling

--- hello_world/main/main.c
+++ hello_world/main/main.c
@@ -2,10 +2,33 @@
 #include <freertos/FreeRTOS.h>
 #include <freertos/task.h>
 
+void hello_world_task(void *pvParameters)
+{
+    while(1) {
+        printf("Hello world!\n");
+        vTaskDelay(250 / portTICK_RATE_MS);
+    }
+
+    vTaskDelete(NULL);
+}
+
+void hello_moon_task(void *pvParameters)
+{
+    while(1) {
+        printf("Hello moon!\n");
+        vTaskDelay(250 / portTICK_RATE_MS);
+    }
+
+    vTaskDelete(NULL);
+}
+
+
 // ESP32 application entry point
 void app_main()
 {
-    printf("Hello world!\n");
+    printf("Starting...\n");
+    xTaskCreatePinnedToCore(hello_world_task, "Hello world task", 1024, NULL, 1, NULL, 1);
+    xTaskCreatePinnedToCore(hello_moon_task, "Hello moon task", 1000, NULL, 1, NULL, 1);
 
     while(1);
 }
