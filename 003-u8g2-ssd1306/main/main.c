1ff77048512bba203580a7fbd54a924ace3da36c
Mika Tuupola <tuupola@appelsiini.net>
2018-04-08 16:31:28 +0700

    Whitespace

--- 003-u8g2-ssd1306/main/main.c
+++ 003-u8g2-ssd1306/main/main.c
@@ -30,15 +30,16 @@ void ssd1306_init()
 {
     ESP_LOGI(TAG, "Initialize, clear and power up the display.");
     u8g2_esp32_hal_t u8g2_esp32_hal = U8G2_ESP32_HAL_DEFAULT;
-	u8g2_esp32_hal.sda = SSD1306_SDA;
-	u8g2_esp32_hal.scl = SSD1306_SCL;
+    u8g2_esp32_hal.sda = SSD1306_SDA;
+    u8g2_esp32_hal.scl = SSD1306_SCL;
     u8g2_esp32_hal_init(u8g2_esp32_hal);
 
-	u8g2_Setup_ssd1306_i2c_128x64_alt0_f(
-		&u8g2,
-		U8G2_R0,
-		u8g2_esp32_i2c_byte_cb,
-		u8g2_esp32_gpio_and_delay_cb);
+    u8g2_Setup_ssd1306_i2c_128x64_alt0_f(
+        &u8g2,
+        U8G2_R0,
+        u8g2_esp32_i2c_byte_cb,
+        u8g2_esp32_gpio_and_delay_cb
+    );
 
     u8x8_SetI2CAddress(&u8g2.u8x8, 0x78);
     u8g2_InitDisplay(&u8g2);
