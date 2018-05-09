eeb63c1b04bce475b575dd780c52e0f1311102df
Mika Tuupola <tuupola@appelsiini.net>
2018-05-09 15:24:39 +0700

    Move fire effect to separate module

--- 010-copepod-demo/main/fire.c
+++ 010-copepod-demo/main/fire.c
@@ -1,6 +1,140 @@
+/*
+
+Adapted from Lode's classic fire tutorial: http://lodev.org/cgtutor/fire.html
+
+Copyright (c) 2004-2007, Lode Vandevenne
+Copyright (c) 2018, Mika Tuupola
+
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions are met:
+
+    * Redistributions of source code must retain the above copyright notice,
+      this list of conditions and the following disclaimer.
+
+    * Redistributions in binary form must reproduce the above copyright
+      notice, this list of conditions and the following disclaimer in the
+      documentation and/or other materials provided with the distribution.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+POSSIBILITY OF SUCH DAMAGE.
+
+*/
+
+#include <stdbool.h>
 #include <stdint.h>
 
-void init_palette(uint16_t *palette[256])
-{
+#include <string.h>
+#include <stdio.h>
 
+#include <blit.h>
+#include <bitmap.h>
+#include <esp_log.h>
+
+#include "color.h"
+#include "byteswap.h"
+#include "bitmap.h"
+#include "fire.h"
+
+static const char *TAG = "fire";
+static uint16_t g_palette[256];
+static uint8_t g_fire[FIRE_HEIGHT][FIRE_WIDTH];
+
+void fire_init(void)
+{
+    uint16_t color;
+    uint8_t r, g, b;
+    hsl_t hsl;
+    rgb_t rgb;
+
+    /* Generate palette. */
+    for (uint16_t x = 0; x < 256; x++) {
+        /* Hue goes from 0 to 85: red to yellow. */
+        /* Saturation is always the maximum: 255. */
+        /* Lightness is 0..255 for x=0..128, and 255 for x=128..255. */
+        hsl.h = x / 3;
+        hsl.s = 255;
+        hsl.l = min(255, x * 2);
+
+        hsl2rgb(&hsl, &rgb);
+
+        color = RGB565(rgb.r, rgb.g, rgb.b);
+        g_palette[x] = bswap_16(color);
+    }
+
+    /* Clear the fire. */
+    memset(g_fire, 0x00, FIRE_HEIGHT * FIRE_WIDTH);
+}
+
+void fire_effect(bitmap_t *dst)
+{
+    uint16_t color;
+
+    /* Random bright line in the bottom. */
+    for (uint16_t x = 0; x < FIRE_WIDTH; x++) {
+        color = abs(32768 + rand());
+        g_fire[FIRE_HEIGHT - 1][x] = color;
+    }
+
+    /* The fire effect itself. */
+    for(uint16_t y = 0; y < FIRE_HEIGHT - 1; y++) {
+        for(uint16_t x = 0; x < FIRE_WIDTH; x++) {
+            g_fire[y][x] =
+                ((g_fire[(y + 1) % FIRE_HEIGHT][(x - 1 + FIRE_WIDTH) % FIRE_WIDTH]
+                + g_fire[(y + 1) % FIRE_HEIGHT][(x) % FIRE_WIDTH]
+                + g_fire[(y + 1) % FIRE_HEIGHT][(x + 1) % FIRE_WIDTH]
+                + g_fire[(y + 2) % FIRE_HEIGHT][(x) % FIRE_WIDTH])
+                * 32) / 129;
+        }
+    }
+
+    /* Copy everything to the destination bitmap. */
+    for(uint16_t y = 0; y < FIRE_HEIGHT - 1; y++) {
+        for(uint16_t x = 0; x < FIRE_WIDTH; x++) {
+            uint16_t *fireptr = dst->buffer + dst->pitch * y + dst->bpp * x;
+            *fireptr = g_palette[g_fire[y][x]];
+        }
+    }
+}
+
+
+void fire_putchar(char ascii, uint16_t x0, uint16_t y0, uint16_t color, char font[128][8], bitmap_t *dst)
+{
+    bool set;
+    bitmap_t font_bitmap = {
+        .width = 8,
+        .height = 8,
+        .depth = 16,
+    };
+
+    bitmap_init(&font_bitmap);
+
+    uint16_t *ptr = font_bitmap.buffer;
+
+    for (uint8_t x = 0; x < 8; x++) {
+        for (uint8_t y = 0; y < 8; y++) {
+            set = font[(uint8_t)ascii][x] & 1 << y;
+            if (set) {
+                *(ptr++) = color;
+            } else {
+                *(ptr++) = 0x0000;
+            }
+        }
+    }
+
+    // ESP_LOGD(TAG, "---");
+    // ESP_LOG_BUFFER_HEXDUMP(TAG, font_bitmap.buffer, 8 * 8 * 2, ESP_LOG_DEBUG);
+
+    blit(x0, y0, &font_bitmap, dst);
+    bitmap_destroy(&font_bitmap);
 }
--- 010-copepod-demo/main/main.c
+++ 010-copepod-demo/main/main.c
@@ -20,91 +20,57 @@ LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE.
 
-***
-
-Contains code from http://lodev.org/cgtutor/fire.html
-
-Copyright (c) 2004-2007, Lode Vandevenne
-
-All rights reserved.
-
-Redistribution and use in source and binary forms, with or without
-modification, are permitted provided that the following conditions are met:
-
-    * Redistributions of source code must retain the above copyright notice,
-      this list of conditions and the following disclaimer.
-
-    * Redistributions in binary form must reproduce the above copyright
-      notice, this list of conditions and the following disclaimer in the
-      documentation and/or other materials provided with the distribution.
-
-THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
-AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
-IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
-ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
-LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
-CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
-SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
-INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
-CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
-ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
-POSSIBILITY OF SUCH DAMAGE.
-
 */
 
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-//#include <math.h>
 
 #include <freertos/FreeRTOS.h>
 #include <freertos/task.h>
-// #include <driver/spi_master.h>
 #include <esp_log.h>
 #include <esp_task_wdt.h>
-
-//#include "fps.h"
-// #include "alien.h"
 
 #include "bitmap.h"
 #include "byteswap.h"
 #include "ili9341.h"
 #include "copepod.h"
 #include "font8x8.h"
+#include "fire.h"
 #include "fps.h"
 #include "fps2.h"
 
 static const char *TAG = "main";
 
-spi_device_handle_t g_spi;
 SemaphoreHandle_t g_mutex;
-float g_fps, g_fps2;
+float g_fps;
+float g_fps2;
 
-#define FB_WIDTH        320
-#define FB_HEIGHT       240
-#define FIRE_WIDTH      160
-#define FIRE_HEIGHT     100
+#define FRAMEBUFFER_WIDTH   320
+#define FRAMEBUFFER_HEIGHT  240
 
-framebuffer_t g_fb = {
-    .width = FB_WIDTH,
-    .height = FB_HEIGHT,
-    .depth = 16,
-};
-
+/*
+ * Flushes the framebuffer to display in a loop. With the ILI9341 DMA
+ * driver can currently do 29 FPS.
+ */
 void framebuffer_task(void *params)
 {
     while (1) {
         xSemaphoreTake(g_mutex, portMAX_DELAY);
-        ili9431_bitmap(g_spi, 0, 0, FB_WIDTH, FB_HEIGHT, g_fb.buffer);
+        pod_flush();
         xSemaphoreGive(g_mutex);
         g_fps = fps();
         //vTaskDelay(1 / portTICK_RATE_MS);
     }
 
     vTaskDelete(NULL);
-
 }
 
+/*
+ * Displays the info bar on top of the screen. Separate FPS values
+ * for the fire effect and the framebuffer. Effect is actually drawn
+ * faster the display is refreshed.
+ */
 void fps_task(void *params)
 {
     uint16_t color;
@@ -123,37 +89,12 @@ void fps_task(void *params)
     vTaskDelete(NULL);
 }
 
-/* http://lodev.org/cgtutor/fire.html */
+/*
+ * Animated fire effect. Adapted from http://lodev.org/cgtutor/fire.html.
+ */
 void demo_task(void *params)
 {
-    uint16_t palette[256];
-    uint8_t fire[FIRE_HEIGHT][FIRE_WIDTH];
-    hsl_t hsl;
-    rgb_t rgb;
-
-    uint16_t x1, y1, x2, y2;
-    uint8_t r, g, b;
-    uint16_t color;
-
-    ESP_LOGI(TAG, "Generating palette.");
-    for (uint16_t x = 0; x < 256; x++) {
-        /* Hue goes from 0 to 85: red to yellow. */
-        /* Saturation is always the maximum: 255. */
-        /* Lightness is 0..255 for x=0..128, and 255 for x=128..255. */
-        hsl.h = x / 3;
-        hsl.s = 255;
-        hsl.l = min(255, x * 2);
-
-        hsl2rgb(&hsl, &rgb);
-
-        color = RGB565(rgb.r, rgb.g, rgb.b);
-        palette[x] = bswap_16(color);
-    }
-
-    /* Clear the fire. */
-    memset(fire, 0x00, FIRE_HEIGHT * FIRE_WIDTH);
-
-    ESP_LOGI(TAG, "Heap before fire bitmap: %d", esp_get_free_heap_size());
+    ESP_LOGI(TAG, "Heap before initialising fire: %d", esp_get_free_heap_size());
 
     bitmap_t bitmap = {
         .width = FIRE_WIDTH,
@@ -161,42 +102,20 @@ void demo_task(void *params)
         .depth = 16,
     };
     bitmap_init(&bitmap);
-
     ESP_LOGI(TAG, "Heap after fire bitmap: %d", esp_get_free_heap_size());
 
+    fire_init();
+    ESP_LOGI(TAG, "Heap after fire init: %d", esp_get_free_heap_size());
+
+
     while (1) {
-        /* Random bright line in the bottom. */
-        for (uint16_t x = 0; x < FIRE_WIDTH; x++) {
-            color = abs(32768 + rand());
-            fire[FIRE_HEIGHT - 1][x] = color;
-        }
-
-        for(uint16_t y = 0; y < FIRE_HEIGHT - 1; y++) {
-            for(uint16_t x = 0; x < FIRE_WIDTH; x++) {
-                fire[y][x] =
-                    ((fire[(y + 1) % FIRE_HEIGHT][(x - 1 + FIRE_WIDTH) % FIRE_WIDTH]
-                    + fire[(y + 1) % FIRE_HEIGHT][(x) % FIRE_WIDTH]
-                    + fire[(y + 1) % FIRE_HEIGHT][(x + 1) % FIRE_WIDTH]
-                    + fire[(y + 2) % FIRE_HEIGHT][(x) % FIRE_WIDTH])
-                    * 32) / 129;
-            }
-        }
-
-        /* Make sure watchdog does not trigger. */
-        esp_task_wdt_reset();
-
-        for(uint16_t y = 0; y < FIRE_HEIGHT - 1; y++) {
-            for(uint16_t x = 0; x < FIRE_WIDTH; x++) {
-                uint16_t *fireptr = bitmap.buffer + bitmap.pitch * y + bitmap.bpp * x;
-                *fireptr = palette[fire[y][x]];
-            }
-        }
-
+        fire_effect(&bitmap);
+        //esp_task_wdt_reset();
         /* Blit the fire bitmap scaled up to framebuffer. */
-        pod_scale_blit(0, 20, 320, 220, &bitmap);
+        pod_scale_blit(0, 20, FRAMEBUFFER_WIDTH, FRAMEBUFFER_HEIGHT - 20, &bitmap);
         g_fps2 = fps2();
 
-        //vTaskDelay(1 / portTICK_RATE_MS);
+        //vTaskDelay(10 / portTICK_RATE_MS);
     }
 
     bitmap_destroy(&bitmap);
@@ -204,34 +123,24 @@ void demo_task(void *params)
     vTaskDelete(NULL);
 }
 
+
+
 void app_main()
 {
     ESP_LOGI(TAG, "SDK version: %s", esp_get_idf_version());
+    ESP_LOGI(TAG, "Heap when starting: %d", esp_get_free_heap_size());
 
-    spi_master_init(&g_spi);
-    ili9341_init(&g_spi);
-    framebuffer_init(&g_fb);
+    pod_init();
+
+    ESP_LOGI(TAG, "Heap after pod init: %d", esp_get_free_heap_size());
 
     g_mutex = xSemaphoreCreateMutex();
 
-    ESP_LOGI(
-        TAG, "bpp: %d width: %d height: %d depth: %d pitch: %d size: %d",
-        g_fb.bpp, g_fb.width, g_fb.height, g_fb.depth, g_fb.pitch, g_fb.size
-    );
-
-    if (NULL == g_fb.buffer) {
-        ESP_LOGE(TAG, "Malloc failed.");
+    if (NULL != g_mutex) {
+        xTaskCreatePinnedToCore(framebuffer_task, "Framebuffer", 8192, NULL, 1, NULL, 0);
+        xTaskCreatePinnedToCore(fps_task, "FPS", 4096, NULL, 2, NULL, 1);
+        xTaskCreatePinnedToCore(demo_task, "Fire", 8192, NULL, 1, NULL, 1);
     } else {
-        ESP_LOGI(TAG, "Heap after framebuffer: %d", esp_get_free_heap_size());
-
-        memset(g_fb.buffer, 0x00, g_fb.size);
-
-        if (NULL != g_mutex) {
-            xTaskCreatePinnedToCore(framebuffer_task, "Framebuffer", 8192, NULL, 1, NULL, 0);
-            xTaskCreatePinnedToCore(fps_task, "FPS", 4096, NULL, 2, NULL, 1);
-            xTaskCreatePinnedToCore(demo_task, "Demo", 76800 + 4096, NULL, 1, NULL, 1);
-        } else {
-            ESP_LOGE(TAG, "No mutex?");
-        }
+        ESP_LOGE(TAG, "No mutex?");
     }
 }
