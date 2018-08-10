84934b6ebdc89cd6bcb88aca170015f3781a5d87
Mika Tuupola <tuupola@appelsiini.net>
2018-08-10 13:11:06 +0300

    Parse RMC sentences with minmea, stubs for others

---
+++ 011-m5stack-gps/components/minmea/component.mk
@@ -0,0 +1,5 @@
+CFLAGS += -Dtimegm=mktime
+COMPONENT_SRCDIRS := minmea
+COMPONENT_ADD_INCLUDEDIRS := minmea
+COMPONENT_PRIV_INCLUDEDIRS :=
+COMPONENT_OBJS := minmea/minmea.o
--- 011-m5stack-gps/main/main.c
+++ 011-m5stack-gps/main/main.c
@@ -32,12 +32,8 @@ SOFTWARE.
 #include <esp_log.h>
 #include <driver/uart.h>
 
+#include "minmea.h"
 #include "sdkconfig.h"
-
-//#define UART_RX_PIN             21
-//  GPIO16-TXD
-// GPIO17-RXD
-// GPIO34-PPS
 
 #define UART_RX_BUF_SIZE    2048
 #define UART_TX_BUF_SIZE    0
@@ -77,8 +73,8 @@ void uart_init()
     ESP_ERROR_CHECK(
         uart_set_pin(
             UART_NUM_2,
-            GPIO_NUM_17, // M5Stack GPS module pins
-            GPIO_NUM_16, // M5Stack GPS module pins
+            GPIO_NUM_17, // M5Stack TX GPS module pins
+            GPIO_NUM_16, // M5Stack RX GPS module pins
             UART_PIN_NO_CHANGE,
             UART_PIN_NO_CHANGE
         )
@@ -99,7 +95,7 @@ char *uart_read_line(uart_port_t uart_port) {
         );
 
         if (1 == bytes_read) {
-            if('\n' == *ptr) {
+            if ('\n' == *ptr) {
                 ptr++;
                 *ptr = '\0';
                 return line;
@@ -121,8 +117,78 @@ void uart_read_task(void *params)
     vTaskDelete(NULL);
 }
 
+void uart_read_and_parse_task(void *params)
+{
+    ESP_LOGI(TAG, "Reading and parsing NMEA from uart...");
+    while(1) {
+        char *line = uart_read_line(UART_NUM_2);
+
+        switch (minmea_sentence_id(line, false)) {
+            /* RMC (Recommended Minimum: position, velocity, time) */
+            case MINMEA_SENTENCE_RMC: {
+                struct minmea_sentence_rmc frame;
+                if (minmea_parse_rmc(&frame, line)) {
+                    ESP_LOGI(
+                        TAG,
+                        "$xxRMC: coordinates and speed: (%f,%f) %f",
+                        minmea_tocoord(&frame.latitude),
+                        minmea_tocoord(&frame.longitude),
+                        minmea_tofloat(&frame.speed)
+                    );
+                } else {
+                    ESP_LOGI(TAG, "$xxRMC sentence is not parsed");
+                }
+            } break;
+
+            /* ZDA (Time & Date - UTC, day, month, year and local time zone) */
+            case MINMEA_SENTENCE_ZDA: {
+                ESP_LOGI(TAG, "$xxZDA");
+            } break;
+
+            /* GGA (Fix Data) */
+            case MINMEA_SENTENCE_GGA: {
+                ESP_LOGI(TAG, "$xxCGA");
+            } break;
+
+            /* VTG (Track made good and Ground speed) */
+            case MINMEA_SENTENCE_VTG: {
+                ESP_LOGI(TAG, "$xxVTG");
+            } break;
+
+            /* GSV (Satellites in view) */
+            case MINMEA_SENTENCE_GSV: {
+                ESP_LOGI(TAG, "$xxGSV");
+            } break;
+
+            /* GST (Pseudorange Noise Statistics) */
+            case MINMEA_SENTENCE_GST: {
+                ESP_LOGI(TAG, "$xxGST");
+            } break;
+
+            /* GLL (Geographic Position: Latitude/Longitude) */
+            case MINMEA_SENTENCE_GLL: {
+                ESP_LOGI(TAG, "$xxGLL");
+            } break;
+
+            /* GSA (DOP and active satellites) */
+            case MINMEA_SENTENCE_GSA: {
+                ESP_LOGI(TAG, "$xxGSA");
+            } break;
+
+            case MINMEA_UNKNOWN: {
+                ESP_LOGI(TAG, "$xxxxx sentence is not valid\n");
+            } break;
+
+            case MINMEA_INVALID: {
+                ESP_LOGI(TAG, "$xxxxx sentence is not valid\n");
+            } break;
+        }
+    }
+    vTaskDelete(NULL);
+}
+
 void app_main()
 {
     uart_init();
-    xTaskCreatePinnedToCore(uart_read_task, "UART read", 2048, NULL, 1, NULL, 1);
+    xTaskCreatePinnedToCore(uart_read_and_parse_task, "UART read and parse", 2048, NULL, 1, NULL, 1);
 }
