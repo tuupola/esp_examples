0f800c558acb70d5110317ac4dbc0ae47a77a583
Mika Tuupola <tuupola@appelsiini.net>
2019-09-20 00:39:39 +0300

    Sync to latest version of copepod

--- 009-m5stack-gfx/main/main.c
+++ 009-m5stack-gfx/main/main.c
@@ -28,6 +28,7 @@ SOFTWARE.
 #include <time.h>
 
 #include <freertos/FreeRTOS.h>
+#include <freertos/semphr.h>
 #include <freertos/task.h>
 #include <esp_log.h>
 #include <esp_task_wdt.h>
@@ -36,6 +37,7 @@ SOFTWARE.
 #include "byteswap.h"
 #include "ili9341.h"
 #include "copepod.h"
+#include "copepod-hal.h"
 #include "font8x8.h"
 #include "fps.h"
 #include "fps2.h"
@@ -48,9 +50,6 @@ static float fb_fps;
 static float fx_fps;
 static uint16_t demo = 0;
 
-#define FRAMEBUFFER_WIDTH   320
-#define FRAMEBUFFER_HEIGHT  240
-
 /*
  * Flushes the framebuffer to display in a loop. With the ILI9341 DMA
  * driver can currently do 29 FPS.
@@ -62,7 +61,6 @@ void framebuffer_task(void *params)
         pod_flush();
         xSemaphoreGive(mutex);
         fb_fps = fps();
-        //vTaskDelay(1 / portTICK_RATE_MS);
     }
 
     vTaskDelete(NULL);
@@ -81,7 +79,7 @@ void fps_task(void *params)
 #ifdef CONFIG_POD_HAL_USE_FRAMEBUFFER
     while (1) {
         sprintf(message, " FX %.*f FPS              FB %.*f FPS", 1, fx_fps, 1, fb_fps);
-        pod_puttext(message, 0, 4, color, font8x8_basic);
+        pod_put_text(message, 0, 4, color, font8x8);
         ESP_LOGI(TAG, "FX %.*f FPS / FB %.*f FPS", 1, fx_fps, 1, fb_fps);
 
         vTaskDelay(1000 / portTICK_RATE_MS);
@@ -90,7 +88,7 @@ void fps_task(void *params)
     while (1) {
         sprintf(message, " FX %.*f FPS", 1, fx_fps);
         //xSemaphoreTake(mutex, portMAX_DELAY);
-        pod_puttext(message, 0, 4, color, font8x8_basic);
+        pod_put_text(message, 0, 4, color, font8x8);
         //xSemaphoreGive(mutex);
         ESP_LOGI(TAG, "FX %.*f FPS", 1, fx_fps);
 
@@ -104,7 +102,7 @@ void switch_task(void *params)
 {
     while (1) {
         //pod_cls();
-        demo = demo + 1;
+        demo = (demo + 1) % 12;
         fx_fps = fps2(true);
         //vTaskDelay(10000 / portTICK_RATE_MS);
         vTaskDelay(3000 / portTICK_RATE_MS);
@@ -113,124 +111,170 @@ void switch_task(void *params)
     vTaskDelete(NULL);
 }
 
+void polygon_demo()
+{
+    int16_t x0 = (rand() % 360) - 20; /* -20 ... 340 */
+    int16_t y0 = (rand() % 280) - 20; /* -20 ... 260 */
+    int16_t x1 = (rand() % 360) - 20; /* -20 ... 340 */
+    int16_t y1 = (rand() % 280) - 20; /* -20 ... 260 */
+    int16_t x2 = (rand() % 360) - 20; /* -20 ... 340 */
+    int16_t y2 = (rand() % 280) - 20; /* -20 ... 260 */
+    int16_t x3 = (rand() % 360) - 20; /* -20 ... 340 */
+    int16_t y3 = (rand() % 280) - 20; /* -20 ... 260 */
+    int16_t x4 = (rand() % 360) - 20; /* -20 ... 340 */
+    int16_t y4 = (rand() % 280) - 20; /* -20 ... 260 */
+    uint16_t colour = rand() % 0xffff;
+    int16_t vertices[10] = {x0, y0, x1, y1, x2, y2, x3, y3, x4, y4};
+    pod_draw_polygon(5, vertices, colour);
+}
+
+void fill_polygon_demo()
+{
+    int16_t x0 = (rand() % 360) - 20; /* -20 ... 340 */
+    int16_t y0 = (rand() % 280) - 20; /* -20 ... 260 */
+    int16_t x1 = (rand() % 360) - 20; /* -20 ... 340 */
+    int16_t y1 = (rand() % 280) - 20; /* -20 ... 260 */
+    int16_t x2 = (rand() % 360) - 20; /* -20 ... 340 */
+    int16_t y2 = (rand() % 280) - 20; /* -20 ... 260 */
+    int16_t x3 = (rand() % 360) - 20; /* -20 ... 340 */
+    int16_t y3 = (rand() % 280) - 20; /* -20 ... 260 */
+    int16_t x4 = (rand() % 360) - 20; /* -20 ... 340 */
+    int16_t y4 = (rand() % 280) - 20; /* -20 ... 260 */
+    uint16_t colour = rand() % 0xffff;
+    int16_t vertices[10] = {x0, y0, x1, y1, x2, y2, x3, y3, x4, y4};
+    pod_fill_polygon(5, vertices, colour);
+}
+
+void circle_demo()
+{
+    int16_t x0 = (rand() % 360) - 20; /* -20 ... 340 */
+    int16_t y0 = (rand() % 280) - 20; /* -20 ... 260 */
+    uint16_t r = (rand() % 40);
+    uint16_t colour = rand() % 0xffff;
+    pod_draw_circle(x0, y0, r, colour);
+}
+
+void fill_circle_demo()
+{
+    int16_t x0 = (rand() % 360) - 20; /* -20 ... 340 */
+    int16_t y0 = (rand() % 280) - 20; /* -20 ... 260 */
+    uint16_t r = (rand() % 40);
+    uint16_t colour = rand() % 0xffff;
+    pod_fill_circle(x0, y0, r, colour);
+}
+
+void line_demo()
+{
+    int16_t x0 = (rand() % 360) - 20; /* -20 ... 340 */
+    int16_t y0 = (rand() % 280) - 20; /* -20 ... 260 */
+    int16_t x1 = (rand() % 360) - 20; /* -20 ... 340 */
+    int16_t y1 = (rand() % 280) - 20; /* -20 ... 260 */
+    uint16_t colour = rand() % 0xffff;
+    pod_draw_line(x0, y0, x1, y1, colour);
+}
+
+void rectangle_demo()
+{
+    int16_t x0 = (rand() % 360) - 20; /* -20 ... 340 */
+    int16_t y0 = (rand() % 280) - 20; /* -20 ... 260 */
+    int16_t x1 = (rand() % 360) - 20; /* -20 ... 340 */
+    int16_t y1 = (rand() % 280) - 20; /* -20 ... 260 */
+    uint16_t colour = rand() % 0xffff;
+    pod_draw_rectangle(x0, y0, x1, y1, colour);
+}
+
+void fill_rectangle_demo()
+{
+    int16_t x0 = (rand() % 360) - 20; /* -20 ... 340 */
+    int16_t y0 = (rand() % 280) - 20; /* -20 ... 260 */
+    int16_t x1 = (rand() % 360) - 20; /* -20 ... 340 */
+    int16_t y1 = (rand() % 280) - 20; /* -20 ... 260 */
+    uint16_t colour = rand() % 0xffff;
+    pod_fill_rectangle(x0, y0, x1, y1, colour);
+}
+
+void put_character_demo()
+{
+    int16_t x0 = (rand() % 360) - 20; /* -20 ... 340 */
+    int16_t y0 = (rand() % 280) - 20; /* -20 ... 260 */
+
+    uint16_t colour = rand() % 0xffff;
+    char ascii = rand() % 127;
+    pod_put_char(ascii, x0, y0, colour, font8x8);
+}
+
+void put_text_demo()
+{
+    int16_t x0 = (rand() % 360) - 20; /* -20 ... 340 */
+    int16_t y0 = (rand() % 280) - 20; /* -20 ... 260 */
+
+    uint16_t colour = rand() % 0xffff;
+    pod_put_text("YO! MTV raps.", x0, y0, colour, font8x8);
+}
+
+void put_pixel_demo()
+{
+    int16_t x0 = (rand() % 360) - 20; /* -20 ... 340 */
+    int16_t y0 = (rand() % 280) - 20; /* -20 ... 260 */
+
+    uint16_t colour = rand() % 0xffff;
+    pod_put_pixel(x0, y0, colour);
+}
+
+void triangle_demo()
+{
+    int16_t x0 = (rand() % 360) - 20; /* -20 ... 340 */
+    int16_t y0 = (rand() % 280) - 20; /* -20 ... 260 */
+    int16_t x1 = (rand() % 360) - 20; /* -20 ... 340 */
+    int16_t y1 = (rand() % 280) - 20; /* -20 ... 260 */
+    int16_t x2 = (rand() % 360) - 20; /* -20 ... 340 */
+    int16_t y2 = (rand() % 280) - 20; /* -20 ... 260 */
+    uint16_t colour = rand() % 0xffff;
+    pod_draw_triangle(x0, y0, x1, y1, x2, y2, colour);
+}
+
+void fill_triangle_demo()
+{
+    int16_t x0 = (rand() % 360) - 20; /* -20 ... 340 */
+    int16_t y0 = (rand() % 280) - 20; /* -20 ... 260 */
+    int16_t x1 = (rand() % 360) - 20; /* -20 ... 340 */
+    int16_t y1 = (rand() % 280) - 20; /* -20 ... 260 */
+    int16_t x2 = (rand() % 360) - 20; /* -20 ... 340 */
+    int16_t y2 = (rand() % 280) - 20; /* -20 ... 260 */
+    uint16_t colour = rand() % 0xffff;
+    pod_fill_triangle(x0, y0, x1, y1, x2, y2, colour);
+}
+
 void demo_task(void *params)
 {
     while (1) {
         //xSemaphoreTake(mutex, portMAX_DELAY);
 
-        //demo = 4;
-
-        if (0 == demo % 10) {
-            /* Random five sided filled polygons, 540 per second. */
-            int16_t x0 = (rand() % 360) - 20; /* -20 ... 360 */
-            int16_t y0 = (rand() % 240) + 20; /*  20 ... 260 */
-            int16_t x1 = (rand() % 360) - 20; /* -20 ... 360 */
-            int16_t y1 = (rand() % 240) + 20; /*  20 ... 260 */
-            int16_t x2 = (rand() % 360) - 20; /* -20 ... 360 */
-            int16_t y2 = (rand() % 240) + 20; /*  20 ... 260 */
-            int16_t x3 = (rand() % 360) - 20; /* -20 ... 360 */
-            int16_t y3 = (rand() % 240) + 20; /*  20 ... 260 */
-            int16_t x4 = (rand() % 360) - 20; /* -20 ... 360 */
-            int16_t y4 = (rand() % 240) + 20; /*  20 ... 260 */
-
-            int16_t vertices[10] = {x0, y0, x1, y1, x2, y2, x3, y3, x4, y4};
-            uint16_t colour = rand() % 0xffff;
-            pod_fillpolygon(5, vertices, colour);
-        } else if (0 == demo % 9) {
-            /* Random five sided polygons, 1900 per second. */
-            int16_t x0 = (rand() % 360) - 20; /* -20 ... 360 */
-            int16_t y0 = (rand() % 240) + 20; /*  20 ... 260 */
-            int16_t x1 = (rand() % 360) - 20; /* -20 ... 360 */
-            int16_t y1 = (rand() % 240) + 20; /*  20 ... 260 */
-            int16_t x2 = (rand() % 360) - 20; /* -20 ... 360 */
-            int16_t y2 = (rand() % 240) + 20; /*  20 ... 260 */
-            int16_t x3 = (rand() % 360) - 20; /* -20 ... 360 */
-            int16_t y3 = (rand() % 240) + 20; /*  20 ... 260 */
-            int16_t x4 = (rand() % 360) - 20; /* -20 ... 360 */
-            int16_t y4 = (rand() % 240) + 20; /*  20 ... 260 */
-
-            int16_t vertices[10] = {x0, y0, x1, y1, x2, y2, x3, y3, x4, y4};
-            uint16_t colour = rand() % 0xffff;
-            pod_polygon(5, vertices, colour);
-        } else if (0 == demo % 8) {
-            /* Random filled circles, 8100 per second. */
-            uint16_t x0 = (rand() % 320);
-            uint16_t y0 = (rand() % 220) + 60;
-            uint16_t r = (rand() % 40);
-            uint16_t colour = rand() % 0xffff;
-            pod_fillcircle(x0, y0, r, colour);
-        } else if (0 == demo % 7) {
-
-            /* Random circles, 3300 per second. */
-            uint16_t x0 = (rand() % 320);
-            uint16_t y0 = (rand() % 220) + 20;
-            uint16_t r = (rand() % 100);
-            uint16_t colour = rand() % 0xffff;
-
-            pod_circle(x0, y0, r, colour);
-
-        } else if (0 == demo % 6) {
-
-            /* Random pixels, 148000 per second. */
-            uint16_t x0 = (rand() % 320);
-            uint16_t y0 = (rand() % 220) + 20;
-            uint16_t colour = rand() % 0xffff;
-
-            pod_putpixel(x0, y0, colour);
-            //vTaskDelay(100 / portTICK_RATE_MS);
-
-        } else if (0 == demo % 5) {
-
-            /* Random lines, 10950 per second. */
-            int16_t x0 = (rand() % 360) - 20; /* -20 ... 360 */
-            int16_t y0 = (rand() % 240) + 20; /*  20 ... 260 */
-            int16_t x1 = (rand() % 360) - 20; /* -20 ... 360 */
-            int16_t y1 = (rand() % 240) + 20; /*  20 ... 260 */
-            uint16_t colour = rand() % 0xffff;
-
-            pod_line(x0, y0, x1, y1, colour);
-
-        } else if (0 == demo % 4) {
-
-            /* Random rectangles, 21370 per second. */
-            int16_t x0 = (rand() % 360) - 20; /* -20 ... 360 */
-            int16_t y0 = (rand() % 240) + 20; /*  20 ... 260 */
-            int16_t x1 = (rand() % 360) - 20; /* -20 ... 360 */
-            int16_t y1 = (rand() % 240) + 20; /*  20 ... 260 */
-            uint16_t colour = rand() % 0xffff;
-
-            pod_rectangle(x0, y0, x1, y1, colour);
-
-        } else if (0 == demo % 3) {
-
-            /* Random filled rectangles, 2090 per second. */
-            int16_t x0 = (rand() % 360) - 20; /* -20 ... 360 */
-            int16_t y0 = (rand() % 240) + 20; /*  20 ... 260 */
-            int16_t x1 = (rand() % 360) - 20; /* -20 ... 360 */
-            int16_t y1 = (rand() % 240) + 20; /*  20 ... 260 */
-            uint16_t colour = rand() % 0xffff;
-
-            pod_fillrectangle(x0, y0, x1, y1, colour);
-
-        } else if (0 == demo % 2) {
-
-            /* Random ascii characters, 35400 per second. */
-            uint16_t x0 = (rand() % 320);
-            uint16_t y0 = (rand() % 220) + 20;
-            uint16_t colour = rand() % 0xffff;
-            char ascii = rand() % 127;
-            pod_putchar(ascii, x0, y0, colour, font8x8_basic);
-
-        } else if (0 == demo % 1) {
-
-            /* String in random positions, 4300 strings per second. */
-            uint16_t x0 = (rand() % 370) - 50 ;
-            uint16_t y0 = (rand() % 220) + 20;
-            uint16_t colour = rand() % 0xffff;
-            pod_puttext("YO! MTV Raps", x0, y0, colour, font8x8_basic);
+        if (0 == demo) {
+            put_text_demo();
+        } else if (1 == demo) {
+            put_character_demo();
+        } else if (2 == demo) {
+            put_pixel_demo();
+        } else if (3 == demo) {
+            fill_triangle_demo();
+        } else if (4 == demo) {
+            triangle_demo();
+        } else if (5 == demo) {
+            fill_rectangle_demo();
+        } else if (6 == demo) {
+            rectangle_demo();
+        } else if (7 == demo) {
+            line_demo();
+        } else if (8 == demo) {
+            circle_demo();
+        } else if (9 == demo) {
+            fill_circle_demo();
+        } else if (10 == demo) {
+            polygon_demo();
+        } else if (11 == demo) {
+            fill_polygon_demo();
         }
-        //xSemaphoreGive(mutex);
-
         /* Update the FX fps counter. */
         fx_fps = fps2(false);
 
@@ -247,7 +291,7 @@ void app_main()
     ESP_LOGI(TAG, "Heap when starting: %d", esp_get_free_heap_size());
 
     pod_init();
-    pod_clip_window(30, 30, 290, 210);
+    pod_set_clip_window(0, 20, DISPLAY_WIDTH - 2, DISPLAY_HEIGHT - 1);
 
     ESP_LOGI(TAG, "Heap after pod init: %d", esp_get_free_heap_size());
 
@@ -257,7 +301,7 @@ void app_main()
 #ifdef CONFIG_POD_HAL_USE_FRAMEBUFFER
         xTaskCreatePinnedToCore(framebuffer_task, "Framebuffer", 8192, NULL, 1, NULL, 0);
 #endif
-        xTaskCreatePinnedToCore(fps_task, "FPS", 8092, NULL, 2, NULL, 1);
+        /* xTaskCreatePinnedToCore(fps_task, "FPS", 8092, NULL, 2, NULL, 1); */
         xTaskCreatePinnedToCore(demo_task, "Demo", 4096, NULL, 1, NULL, 1);
         xTaskCreatePinnedToCore(switch_task, "Switch", 2048, NULL, 2, NULL, 0);
     } else {
