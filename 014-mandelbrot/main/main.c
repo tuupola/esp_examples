eedef711b2ae2a7f2629f07a0539c9ffc849d920
Mika Tuupola <tuupola@appelsiini.net>
2019-09-27 22:30:35 +0300

    Clean up some code

--- 014-mandelbrot/main/main.c
+++ 014-mandelbrot/main/main.c
@@ -1,6 +1,6 @@
 /*
 
-Copyright (c) 2018 Mika Tuupola
+Copyright (c) 2018-2019 Mika Tuupola
 
 Permission is hereby granted, free of charge, to any person obtaining a copy
 of this software and associated documentation files (the "Software"), to deal
@@ -22,59 +22,17 @@ SOFTWARE.
 
 */
 
-// #include <stdio.h>
-// #include <stdlib.h>
-// #include <string.h>
-#include <time.h>
-
+#include <esp_log.h>
 #include <freertos/FreeRTOS.h>
 #include <freertos/task.h>
 #include <freertos/semphr.h>
+#include <time.h>
 
-//#include <esp_task_wdt.h>
-
-#include <esp_log.h>
-// #include <esp_task_wdt.h>
-
-// #include "bitmap.h"
-// #include "byteswap.h"
-// #include "ili9341.h"
 #include "copepod.h"
-// #include "font8x8.h"
-// #include "fire.h"
-#include "fps.h"
-// #include "fps2.h"
 
 static const char *TAG = "main";
-
 static SemaphoreHandle_t mutex;
-static float fb_fps;
-//static float fx_fps;
 
-// #define FRAMEBUFFER_WIDTH   320
-// #define FRAMEBUFFER_HEIGHT  240
-
-/*
- * Flushes the framebuffer to display in a loop. With the ILI9341 DMA
- * driver can currently do around 29 FPS.
- */
-void framebuffer_task(void *params)
-{
-    while (1) {
-        xSemaphoreTake(mutex, portMAX_DELAY);
-        pod_flush();
-        xSemaphoreGive(mutex);
-        fb_fps = fps();
-        //vTaskDelay(1 / portTICK_RATE_MS);
-    }
-
-    vTaskDelete(NULL);
-}
-
-/*
- * Flushes the framebuffer to display in a loop. With the ILI9341 DMA
- * driver can currently do around 29 FPS.
- */
 void mandelbrot_task(void *params)
 {
     uint16_t max_x = 320;
@@ -127,51 +85,29 @@ void mandelbrot_task(void *params)
                     n++;
                 }
                 if (n < max_iters) {
-                    pod_putpixel(px, py, n * 2);
-                    pod_putpixel(px, max_y - py, n * 2);
+                    pod_put_pixel(px, py, n * 2);
+                    pod_put_pixel(px, max_y - py, n * 2);
                 } else {
-                    pod_putpixel(px, py, 0);
-                    pod_putpixel(px, max_y - py, 0);
+                    pod_put_pixel(px, py, 0);
+                    pod_put_pixel(px, max_y - py, 0);
                 }
             }
         }
         time_spent = (double)(clock() - start) / CLOCKS_PER_SEC;
-        printf("Generated in %g seconds.\n", time_spent);
+        ESP_LOGI(TAG, "Generated in %.2f seconds.", time_spent);
 
         start = clock();
         xSemaphoreTake(mutex, portMAX_DELAY);
         pod_flush();
         xSemaphoreGive(mutex);
         time_spent = (double)(clock() - start) / CLOCKS_PER_SEC;
-        printf("Flushed in %g seconds.\n", time_spent);
+        ESP_LOGI(TAG, "Flushed in %.2f seconds.", time_spent);
 
         zoom = zoom + 0.1;
     }
 
     vTaskDelete(NULL);
 }
-
-/*
- * Displays the info bar on top of the screen. Separate FPS values
- * for the fire effect and the framebuffer. Effect is actually drawn
- * faster the display is refreshed.
-//  */
-// void fps_task(void *params)
-// {
-//     uint16_t color;
-//     char message[42];
-
-//     while (1) {
-//         color = RGB565(0, 0, 255);
-//         sprintf(message, " FX %.*f FPS                FB %.*f FPS", 1, fx_fps, 1, fb_fps);
-//         pod_puttext(message, 0, 4, color, font8x8_basic);
-
-//         ESP_LOGI(TAG, "FX %.*f FPS / FB %.*f FPS", 1, fx_fps, 1, fb_fps);
-
-//         vTaskDelay(1000 / portTICK_RATE_MS);
-//     }
-//     vTaskDelete(NULL);
-// }
 
 void app_main()
 {
@@ -185,9 +121,7 @@ void app_main()
     mutex = xSemaphoreCreateMutex();
 
     if (NULL != mutex) {
-        //xTaskCreatePinnedToCore(framebuffer_task, "FB", 8192, NULL, 1, NULL, 0);
         xTaskCreatePinnedToCore(mandelbrot_task, "Mandelbrot", 8192, NULL, 1, NULL, 0);
-        //xTaskCreatePinnedToCore(fps_task, "FPS", 2048, NULL, 2, NULL, 1);
     } else {
         ESP_LOGE(TAG, "No mutex?");
     }
