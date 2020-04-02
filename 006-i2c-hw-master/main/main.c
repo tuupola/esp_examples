469e5693460be92578030b294a941311a59e8222
Mika Tuupola <tuupola@appelsiini.net>
2020-04-02 13:27:51 +0700

    Relicense to MIT-0

--- 006-i2c-hw-master/main/main.c
+++ 006-i2c-hw-master/main/main.c
@@ -1,19 +1,20 @@
 /*
 
-Copyright (c) 2018 Mika Tuupola
+SPDX-License-Identifier: MIT-0
+
+MIT No Attribution
+
+Copyright (c) 2018-2020 Mika Tuupola
 
 Permission is hereby granted, free of charge, to any person obtaining a copy
 of this software and associated documentation files (the "Software"), to deal
 in the Software without restriction, including without limitation the rights
 to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
 copies of the Software, and to permit persons to whom the Software is
-furnished to do so, subject to the following conditions:
+furnished to do so.
 
-The above copyright notice and this permission notice shall be included in all
-copies or substantial portions of the Software.
-
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
+OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
 FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
 AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
 LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
@@ -59,7 +60,7 @@ void i2c_read_slave_task(void *params)
         i2c_read(TELE_DEVICE_ALTITUDE, data_read, SLAVE_2_DATA_LENGTH);
         ESP_LOG_BUFFER_HEX("0x12", data_read, SLAVE_2_DATA_LENGTH);
 
-        vTaskDelay(2000 / portTICK_RATE_MS);
+        vTaskDelay(1500 / portTICK_RATE_MS);
     }
 
     vTaskDelete(NULL);
